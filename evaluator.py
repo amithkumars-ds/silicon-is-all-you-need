d6087dc878f53c562f0689b5a3df6d43cfbacd92
titan_exasaur <tinytachyon14341@gmail.com>
2026-06-06 01:31:57 +0530

    phase 3: evalutor complete

---
+++ evaluator.py
@@ -0,0 +1,24 @@
+from lexer import TokenType
+from parser import BinOpNode, NumberNode
+
+class Evaluator:
+    def visit(self, node):
+        if isinstance(node, NumberNode):
+            return node.value
+        if isinstance(node, BinOpNode):
+            left = self.visit(node.left)
+            right = self.visit(node.right)
+            if node.op.type == TokenType.PLUS:
+                return left + right
+
+if __name__ == '__main__':
+    from lexer import Lexer
+    from parser import Parser
+
+    lexer = Lexer('3+4')
+    tokens = lexer.tokenize()
+    ast = Parser(tokens).parse()
+
+    evaluator = Evaluator()
+    result = evaluator.visit(ast)
+    print(result)
