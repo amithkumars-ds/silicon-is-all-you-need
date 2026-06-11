aae043be7b6e6d9961d9e4029ce69b2cad3f5999
titan_exasaur <tinytachyon14341@gmail.com>
2026-06-12 02:24:42 +0530

    added if else lexing ability
    - added if, else, comparison token types in TokenType class
    - added peek method in Lexer for peeking the next element
    - added ==, !=, >, >=, <, <= ability for lexer get_next_token method

--- lexer.py
+++ lexer.py
@@ -10,6 +10,21 @@ class TokenType:
     LPAREN = 'LPAREN'
     RPAREN = 'RPAREN'
     COMMA = 'COMMA'
+    IF = 'IF'
+    ELSE = 'ELSE'
+    GREATER_THAN = 'GREATER_THAN'
+    GREATER_THAN_EQUAL = 'GREATER_THAN_EQUAL'
+    LESSER_THAN = 'LESSER_THAN'
+    LESSER_THAN_EQUAL = 'LESSER_THAN_EQUAL'
+    COMPARISON_EQUALS = 'COMPARISON_EQUALS'
+    COMPARISON_NOT_EQUALS = 'COMPARISON_NOT_EQUALS'
+    NOT = 'NOT'
+
+class constants:
+    KEYWORDS = {
+                'if': TokenType.IF,
+                'else': TokenType.ELSE
+                }
 
 class Token:
     def __init__(self, type, value):
@@ -31,6 +46,12 @@ class Lexer:
             self.current_char = self.text[self.pos]
         else:
             self.current_char = None
+
+    def peek(self):
+        peek_pos = self.pos + 1
+        if peek_pos < len(self.text):
+            return self.text[peek_pos]
+        return None
         
     def skip_white_space(self,):
         while self.current_char is not None and self.current_char == ' ':
@@ -48,7 +69,8 @@ class Lexer:
         while self.current_char is not None and self.current_char.isalnum():
             result += self.current_char
             self.advance()
-        return Token(TokenType.IDENTIFIER, str(result))
+        token_type = constants.KEYWORDS.get(result, TokenType.IDENTIFIER)
+        return Token(token_type, result)
 
     def get_next_token(self):
         while self.current_char is not None:
@@ -70,8 +92,18 @@ class Lexer:
                 self.advance()
                 return Token(TokenType.DIVIDE, '/')
             if self.current_char == '=':
+                if self.peek() == '=':
+                    self.advance()
+                    self.advance()
+                    return Token(TokenType.COMPARISON_EQUALS, '==')
                 self.advance()
                 return Token(TokenType.ASSIGN, '=')
+            if self.current_char == '!':
+                if self.peek() == '=':
+                    self.advance()
+                    self.advance()
+                    return Token(TokenType.COMPARISON_NOT_EQUALS, '!=')
+                return Token(TokenType.NOT, '!')
             if self.current_char == '(':
                 self.advance()
                 return Token(TokenType.LPAREN, '(')
@@ -80,6 +112,20 @@ class Lexer:
                 return Token(TokenType.RPAREN, ')')
             if self.current_char.isalpha():
                 return self.identifier()
+            if self.current_char == '>':
+                if self.peek() == '=':
+                    self.advance()
+                    self.advance()
+                    return Token(TokenType.GREATER_THAN_EQUAL, '>=')
+                self.advance()
+                return Token(TokenType.GREATER_THAN, '>')
+            if self.current_char == '<':
+                if self.peek() == '=':
+                    self.advance()
+                    self.advance()
+                    return Token(TokenType.LESSER_THAN_EQUAL, '<=')
+                self.advance()
+                return Token(TokenType.LESSER_THAN, '<')
             if self.current_char == ',':
                 self.advance()
                 return Token(TokenType.COMMA, ',')
@@ -93,3 +139,18 @@ class Lexer:
         tokens.append(Token(TokenType.EOF, None))
         
         return tokens
+    
+
+
+if __name__ == '__main__':
+    tests = [
+        'x == 5',
+        'x != 5',
+        'x >= 5',
+        'x <= 5',
+        'if x > 5',
+        'else',
+    ]
+    for test in tests:
+        lexer = Lexer(test)
+        print(lexer.tokenize())
